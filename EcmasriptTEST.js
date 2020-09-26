12f0caf48060c1c66a8c5d3c671793cb73ae52e1
Hanieh Ghaderi <hanieh.ghaeri11@gmail.com>
2020-09-26 22:40:58 +0330

    added module

--- EcmasriptTEST.js
+++ EcmasriptTEST.js
@@ -21,6 +21,23 @@ class print{
 }
 new print(15);
 
+
+class Polygon { 
+    constructor(height, width) { 
+       this.h = height; 
+       this.w = width;
+    } 
+   
+    test() { 
+       console.log("The height of the polygon: ", this.h); 
+       console.log("The width of the polygon: ",this. w) ;
+    } 
+ } 
+  
+ //creating an instance  
+ var polyObj = new Polygon(10,20); 
+ polyObj.test();
+
 class print{
     constructor(value){
         value.forEach(function(element){
---
+++ module.js
@@ -0,0 +1,5 @@
+   class  print {
+    constructor(){
+        console.log("Hello world")
+    }
+}
